f2a82ba35e03c968476892c732939dd61ab183cf
Your Name <393963771@qq.com>
2017-12-05 15:25:46 +0800

    12/5

--- src/main/java/Solution23.java
+++ src/main/java/Solution23.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 /**
  * created  by KDF on 2017/12/1.
  */
 
 
 public class Solution23 {
+    public static String ReverseSentence(String str){
+        System.out.println(str);
+        String[] a = str.split(str);
+        for(int i= 0;i<a.length;i++) {
+            System.out.println(a[i]);
+        }
+        String temp =null;
+        for(int i =0;i<a.length/2;i++){
+            temp = a[i];
+            a[i] = a[a.length-i-1];
+            a[a.length-i-1] = temp;
+        }
+        for(int i= 0;i<a.length;i++) {
+            System.out.println(a[i]);
+        }
+        return a.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String st  = scanner.nextLine();
+        String a =ReverseSentence(st);
+        System.out.println(a);
+
+
+    }
 }
