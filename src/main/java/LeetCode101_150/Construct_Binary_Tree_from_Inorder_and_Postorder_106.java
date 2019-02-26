0ec557c74a026b9471161c1059d924bce6757ce3
kangdengfei <393963771@qq.com>
2019-02-26 18:59:00 +0800

    add some problems

--- src/main/java/LeetCode101_150/Construct_Binary_Tree_from_Inorder_and_Postorder_106.java
+++ src/main/java/LeetCode101_150/Construct_Binary_Tree_from_Inorder_and_Postorder_106.java
@@ -1,5 +1,25 @@
 package LeetCode101_150;
 
+
+/**
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ *
+ * Note:
+ * You may assume that duplicates do not exist in the tree.
+ *
+ * For example, given
+ *
+ * inorder = [9,3,15,20,7]
+ * postorder = [9,15,7,20,3]
+ *
+ * Return the following binary tree:
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ */
 public class Construct_Binary_Tree_from_Inorder_and_Postorder_106 {
     public static TreeNode buildTree(int[] inorder, int[] postorder) {
         if(inorder.length != postorder.length || inorder == null ||postorder == null){
