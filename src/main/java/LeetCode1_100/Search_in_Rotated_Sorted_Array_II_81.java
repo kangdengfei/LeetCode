8d47074cdcd208921ec00e3174962964ac5d7c36
kangdengfei <393963771@qq.com>
2019-03-15 10:13:03 +0800

    add some problems

--- src/main/java/LeetCode1_100/Search_in_Rotated_Sorted_Array_II_81.java
+++ src/main/java/LeetCode1_100/Search_in_Rotated_Sorted_Array_II_81.java
@@ -23,15 +23,30 @@ package LeetCode1_100;
  **/
 public class Search_in_Rotated_Sorted_Array_II_81 {
     public boolean search(int[] nums, int target) {
+        if (nums == null){
+            return false;
+        }
         int start = 0;
-        int end = nums.length;
-        while (start < end){
+        int end = nums.length-1;
+        while (start <= end){
             int mid = start + (end - start)/2;
             if (nums[mid] == target){
                 return true;
             }
-            if (nums[mid] > nums[start]){
-
+            if (nums[mid] ==  nums[ start]){
+                start++;
+            }else if (nums[start] < nums[mid]){
+                if (nums[start] <= target && target < nums [mid]){
+                    end = mid -1;
+                }else {
+                    start = mid + 1;
+                }
+            }else {
+                if (nums[mid] < target && target <= nums[end]){
+                    start = mid + 1;
+                }else {
+                    end = mid - 1;
+                }
             }
         }
         return false;
